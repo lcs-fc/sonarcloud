fdd4f2126dd02ea1741c844de43f27f79eb8fee3
Leomar Camargo de Souza <leomarcamargodesouza@gmail.com>
2024-01-27 21:36:41 -0300

    ci: new tests

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -9,3 +9,28 @@ func TestSum(t *testing.T) {
 		t.Error("O resultado deveria ser 5")
 	}
 }
+
+
+func TestSub(t *testing.T) {
+	result := Sub(3, 3)
+
+	if result != 0 {
+		t.Error("O resultado deveria ser 0")
+	}
+}
+
+func TestTimes(t *testing.T) {
+	result := Times(2, 3)
+
+	if result != 6 {
+		t.Error("O resultado deveria ser 6")
+	}
+}
+
+func TestSumX(t *testing.T) {
+	result := SumX(2, 3)
+
+	if result != 7 {
+		t.Error("O resultado deveria ser 7")
+	}
+}
